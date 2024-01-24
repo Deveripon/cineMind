03dee8c15e13b62a744759e3bfc583d4a1f60c1e
deveripon <developershahadathussainripon@gmail.com>
2024-01-24 21:00:04 +0600

    dark mode enabled

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -4,19 +4,6 @@ export const ThemeContext = createContext(true);
 export default function ThemeContextProvider({ children }) {
     const [isDark, setIsDark] = useState(true);
 
-    // On page load or when changing themes, best to add inline in `head` to avoid FOUC
-
-    if (localStorage.theme === "dark") {
-        document.documentElement.classList.add("dark");
-    } else {
-        document.documentElement.classList.remove("dark");
-    }
-    if (isDark === true) {
-        localStorage.theme = "dark";
-    } else {
-        localStorage.theme = "light";
-    }
-
     return (
         <>
             <ThemeContext.Provider value={{ isDark, setIsDark }}>
